c71e4e744c10626f765834ff2b2e0cb040ac862d
Chris Richardson <chris@bpi.cam.ac.uk>
2017-01-31 15:36:47 +0000

    Add some more to pybind11

--- pybind11/ROL.cpp
+++ pybind11/ROL.cpp
@@ -3,21 +3,34 @@
 namespace py = pybind11;
 
 #include <ROL_StdVector.hpp>
+#include <ROL_Objective.hpp>
+#include <ROL_Algorithm.hpp>
 
 PYBIND11_PLUGIN(ROL)
 {
-
   py::module m("ROL", "pybind11 ROL plugin");
 
+  // ROL::StdVector<double>
+  //
   py::class_<ROL::StdVector<double>>(m, "StdVector")
     .def("__init__",
          [](ROL::StdVector<double> &instance, int n) {
            Teuchos::RCP<std::vector<double>> tp = Teuchos::rcp<std::vector<double>>(new std::vector<double>(n));
            new (&instance) ROL::StdVector<double>(tp);
          })
+    .def("norm", &ROL::StdVector<double>::norm)
     .def("dimension", &ROL::StdVector<double>::dimension);
 
+  // ROL::Objective<double>
+  //
+  py::class_<ROL::Objective<double>>(m, "Objective")
+    .def("value", &ROL::Objective<double>::value)
+    .def("gradient", &ROL::Objective<double>::gradient);
+
+  // ROL::Algorithm<double>
+  //
+  py::class_<ROL::Algorithm<double>>(m, "Algorithm")
+    .def(py::init<const std::string&, Teuchos::ParameterList&>());
 
   return m.ptr();
-
 }
--- pybind11/test.py
+++ pybind11/test.py
@@ -4,3 +4,13 @@ print dir(ROL)
 
 v = ROL.StdVector(20)
 print v.dimension()
+
+class GreatObjective(ROL.Objective):
+    def __init__(self):
+        pass
+    def value(self, x, tol):
+        print x, tol
+        print x.norm()
+
+w = GreatObjective()
+print w.value(v, 0.1)
