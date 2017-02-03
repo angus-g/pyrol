26b441d5d95682e166e7e134fee916d44eaa4fce
Florian Wechsung <f.wechsung@gmail.com>
2017-02-03 14:22:50 +0000

    something is working!

--- pybind11/ROL.cpp
+++ pybind11/ROL.cpp
@@ -2,14 +2,140 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
+#include <pybind11/functional.h>
 namespace py = pybind11;
 
 #include <ROL_StdVector.hpp>
 #include <ROL_Objective.hpp>
 #include <ROL_Algorithm.hpp>
+#include "Teuchos_RCPStdSharedPtrConversions.hpp"
 
 #include "EigenVector.h"
+class CustomLA : public ROL::Vector<double>
+{
+	public:
+		CustomLA()
+		{
+		}
+		//CustomLA(std::function<void(CustomLA, CustomLA)> iplus, std::function<void(CustomLA, double)> iscale)
+		//{
+		//    _iplus = iplus;
+		//    _iscale = iscale;
+		//}
 
+		virtual void plus(const ROL::Vector<double>& x)
+		{
+			const CustomLA& xx = Teuchos::dyn_cast<const CustomLA>(x);
+			iplus(xx);
+		}
+
+		virtual void iplus(const CustomLA& xx)
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "iplus");
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(xx).cast<void>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'iplus'.");
+		}
+
+		virtual void scale(const double alpha)
+		{
+			std::cout << "here" << std::endl;
+			iscale(alpha);
+		}
+		virtual void iscale(const double alpha)
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "iscale");
+			std::cout << "here" << std::endl;
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(alpha).cast<void>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'iscale'.");
+		}
+
+		virtual double dot(const ROL::Vector<double> &x) const
+		{
+			const CustomLA& xx = Teuchos::dyn_cast<const CustomLA>(x);
+			return 0;
+		}
+
+		virtual double norm() const
+		{
+			return std::sqrt(this->dot(*this));
+		}
+
+		//virtual int dimension() const {
+		//    return _vec->size();
+		//}
+
+		virtual Teuchos::RCP<ROL::Vector<double>> clone() const
+		{
+			return iclone();
+		}
+
+		virtual Teuchos::RCP<CustomLA> iclone() const
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "iclone");
+			if (overload)
+			{
+				py::object res = overload.operator()<py::return_value_policy::reference>();
+				return res.cast<Teuchos::RCP<CustomLA>>();
+			}
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'iclone'.");
+		}
+
+};
+
+class PyCustomLA : public CustomLA
+{
+	public:
+		PyCustomLA()
+			:CustomLA(){}
+
+		virtual void copy_data_from(const CustomLA& y)
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "copy_data_from");
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(y).cast<void>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'copy_data_from'.");
+		}
+
+		virtual void iplus(const CustomLA& xx)
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "iplus");
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(xx).cast<void>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'iplus'.");
+		}
+
+		virtual void iscale(const double alpha)
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "iscale");
+			std::cout << "here" << std::endl;
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(alpha).cast<void>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'iscale'.");
+		}
+		virtual double idot(const CustomLA& xx) const
+		{
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "idot");
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(xx).cast<double>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'idot'.");
+		}
+};
 class PyObjective : public ROL::Objective<double>
 {
 	public:
@@ -35,6 +161,7 @@ class PyObjective : public ROL::Objective<double>
 		}
 };
 
+PYBIND11_DECLARE_HOLDER_TYPE(Z, Teuchos::RCP<Z>);
 
 PYBIND11_PLUGIN(ROL)
 {
@@ -62,6 +189,19 @@ PYBIND11_PLUGIN(ROL)
 		{
 	      (*vvec)[idx] = val;
 		}
+	  }
+	)
+	.def("__getitem__", [](ROL::StdVector<double> &vec, const py::slice& slice)
+	  {
+	    Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
+		py::size_t start, stop, step, slicelength;
+        if (!slice.compute((py::size_t)(vvec->size()), &start, &stop, &step, &slicelength))
+          throw py::error_already_set();
+	    auto res = std::make_shared<std::vector<double>>();
+		res->resize(slicelength);
+	    for (int i = start; i < stop; i=i+step) {
+          res->push_back((*vvec)[i]);
+	    }
 	  }
 	)
 	.def("__getitem__", [](ROL::StdVector<double> &vec, const int& idx)
@@ -110,6 +250,29 @@ PYBIND11_PLUGIN(ROL)
 	  }
 	)
 	.def("scale", &EigenVector::scale);
+
+  //py::class_<CustomLA, ROL::Vector<double>>(m, "CustomLA")
+  py::class_<CustomLA, Teuchos::RCP<CustomLA>>(m, "CustomLA")
+	//.def(py::init<std::function<void(CustomLA, CustomLA)>, std::function<void(CustomLA, double)>>())
+	.def(py::init<>())
+	//.def("norm", &EigenVector::norm)
+	//.def("dimension", &EigenVector::dimension)
+	.def("plus", &CustomLA::plus)
+	.def("scale", &CustomLA::scale)
+	.def("iclone", &CustomLA::iclone)
+	.def("clone", &CustomLA::clone, py::return_value_policy::reference)
+	.def("checkVector", [](CustomLA* x, CustomLA* y, CustomLA* z)
+	  {
+	    x->checkVector(*y, *z, true, std::cout);
+	  })
+    .def("checkClone", [](CustomLA* instance) 
+	  {
+	    instance->scale(0.5);
+	    instance->iclone()->scale(0.5);
+		
+	  });
+	//.def("idot", &CustomLA::idot)
+	//.def("copy_data_from", &CustomLA::copy_data_from);
 
   // ROL::Objective<double>
   //
--- pybind11/pybind_test.py
+++ pybind11/pybind_test.py
@@ -12,37 +12,79 @@ class MyObj(ROL.Objective):
 obj = MyObj()
 algo = ROL.Algorithm("Line Search", {"test":"test"})
 
-x = ROL.StdVector(2)
-x[0] = -1.0
-x[1] = 2.0
+# x = ROL.StdVector(2)
+# x[0] = -1.0
+# x[1] = 2.0
 
-print x[0] 
-print x[1] 
-algo.run(x, obj)
-print x[0] 
-print x[1] 
+# print x[0] 
+# print x[1] 
+# algo.run(x, obj)
+# print x[0] 
+# print x[1] 
 
 
-_x = ROL.EigenVector(2)
-_x[0] = -1.0
-_x[1] = 2.0
+# _x = ROL.EigenVector(2)
+# _x[0] = -1.0
+# _x[1] = 2.0
 
-print _x[0] 
-print _x[1] 
-algo = ROL.Algorithm("Line Search", {"test":"test"})
-algo.run(_x, obj)
-print _x[0] 
-print _x[1] 
+# print _x[0] 
+# print _x[1] 
+# algo = ROL.Algorithm("Line Search", {"test":"test"})
+# algo.run(_x, obj)
+# print _x[0] 
+# print _x[1] 
 
+# import numpy as np
+# a = np.asarray([0, 1, 2, 3])
+# evec = ROL.EigenVector(a)
+# print evec[0]
+# print evec[1]
+# print evec[2]
+# print evec[3]
+# a[3] = 5.0
+# print evec[3]
+# evec[3]=3.1
+# print a[3]
+
+# import ROL
 import numpy as np
-a = np.asarray([0, 1, 2, 3])
-evec = ROL.EigenVector(a)
-print evec[0]
-print evec[1]
-print evec[2]
-print evec[3]
-a[3] = 5.0
-print evec[3]
-evec[3]=3.1
-print a[3]
+def iplus(x, y):
+    x.data[0] += y.data[0]
+    x.data[1] += y.data[1]
 
+def iscale(x, alpha):
+    x.data[0] *= alpha
+
+class NPBasedLA(ROL.CustomLA):
+    def __init__(self):
+        ROL.CustomLA.__init__(self)
+        # super(ROL.CustomLA, self).__init__(self)
+        self.data = np.asarray([0, 0])
+        self.clones = []
+        print "construct"
+
+    def iplus(self, xx):
+        print "here plus"
+
+    def iscale(self, alpha):
+        print "here scale"
+
+    def iclone(self):
+        print "here clone"
+        res = NPBasedLA()
+        self.clones.append(res)
+        # res.iscale(1.0)
+        return res
+
+x = NPBasedLA()
+y = NPBasedLA()
+z = NPBasedLA()
+# x.scale(3.0)
+# x.plus(y)
+# # xx = x.clone()
+# # xx.scale(1.0)
+# yy = x.iclone()
+# yy.iscale(1.0)
+x.checkClone()
+x.checkVector(y, z)
+# # algo.run(x, obj)
