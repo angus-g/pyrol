47e416ec1704deec63bfdaad63610a013edcebe8
florianwechsung <f.wechsung@gmail.com>
2017-02-03 17:37:37 +0000

    had to manually increase ref count but can now define linear algebra in python

--- pybind11/ROL.cpp
+++ pybind11/ROL.cpp
@@ -3,6 +3,7 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 #include <pybind11/functional.h>
+#include <Python.h>
 namespace py = pybind11;
 
 #include <ROL_StdVector.hpp>
@@ -11,54 +12,43 @@ namespace py = pybind11;
 #include "Teuchos_RCPStdSharedPtrConversions.hpp"
 
 #include "EigenVector.h"
-class CustomLA : public ROL::Vector<double>
+class CustomLA : public std::enable_shared_from_this<CustomLA>, public ROL::Vector<double>
 {
 	public:
 		CustomLA()
 		{
 		}
-		//CustomLA(std::function<void(CustomLA, CustomLA)> iplus, std::function<void(CustomLA, double)> iscale)
-		//{
-		//    _iplus = iplus;
-		//    _iscale = iscale;
-		//}
 
 		virtual void plus(const ROL::Vector<double>& x)
 		{
 			const CustomLA& xx = Teuchos::dyn_cast<const CustomLA>(x);
-			iplus(xx);
-		}
-
-		virtual void iplus(const CustomLA& xx)
-		{
 			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "iplus");
+			pybind11::function overload = py::get_overload(this, "plus");
 			if (overload)
 				return overload.operator()<py::return_value_policy::reference>(xx).cast<void>();
 			else
-				py::pybind11_fail("Tried to call pure virtual function 'iplus'.");
+				py::pybind11_fail("Tried to call pure virtual function 'plus'.");
 		}
 
 		virtual void scale(const double alpha)
 		{
-			std::cout << "here" << std::endl;
-			iscale(alpha);
-		}
-		virtual void iscale(const double alpha)
-		{
 			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "iscale");
-			std::cout << "here" << std::endl;
+			pybind11::function overload = py::get_overload(this, "scale");
 			if (overload)
 				return overload.operator()<py::return_value_policy::reference>(alpha).cast<void>();
 			else
-				py::pybind11_fail("Tried to call pure virtual function 'iscale'.");
+				py::pybind11_fail("Tried to call pure virtual function 'scale'.");
 		}
 
 		virtual double dot(const ROL::Vector<double> &x) const
 		{
 			const CustomLA& xx = Teuchos::dyn_cast<const CustomLA>(x);
-			return 0;
+			py::gil_scoped_acquire gil;
+			pybind11::function overload = py::get_overload(this, "dot");
+			if (overload)
+				return overload.operator()<py::return_value_policy::reference>(xx).cast<double>();
+			else
+				py::pybind11_fail("Tried to call pure virtual function 'dot'.");
 		}
 
 		virtual double norm() const
@@ -72,76 +62,25 @@ class CustomLA : public ROL::Vector<double>
 
 		virtual Teuchos::RCP<ROL::Vector<double>> clone() const
 		{
-			return iclone();
-		}
-
-		virtual Teuchos::RCP<CustomLA> iclone() const
-		{
 			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "iclone");
+			pybind11::function overload = py::get_overload(this, "clone");
 			if (overload)
 			{
-				py::object res = overload.operator()<py::return_value_policy::reference>();
-				return res.cast<Teuchos::RCP<CustomLA>>();
+				auto res = overload.operator()<py::return_value_policy::reference>();
+				Py_INCREF(res.ptr());
+				return Teuchos::rcp(res.cast<std::shared_ptr<CustomLA>>());
 			}
 			else
-				py::pybind11_fail("Tried to call pure virtual function 'iclone'.");
+				py::pybind11_fail("Tried to call pure virtual function 'clone'.");
 		}
 
 };
 
-class PyCustomLA : public CustomLA
-{
-	public:
-		PyCustomLA()
-			:CustomLA(){}
-
-		virtual void copy_data_from(const CustomLA& y)
-		{
-			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "copy_data_from");
-			if (overload)
-				return overload.operator()<py::return_value_policy::reference>(y).cast<void>();
-			else
-				py::pybind11_fail("Tried to call pure virtual function 'copy_data_from'.");
-		}
-
-		virtual void iplus(const CustomLA& xx)
-		{
-			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "iplus");
-			if (overload)
-				return overload.operator()<py::return_value_policy::reference>(xx).cast<void>();
-			else
-				py::pybind11_fail("Tried to call pure virtual function 'iplus'.");
-		}
-
-		virtual void iscale(const double alpha)
-		{
-			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "iscale");
-			std::cout << "here" << std::endl;
-			if (overload)
-				return overload.operator()<py::return_value_policy::reference>(alpha).cast<void>();
-			else
-				py::pybind11_fail("Tried to call pure virtual function 'iscale'.");
-		}
-		virtual double idot(const CustomLA& xx) const
-		{
-			py::gil_scoped_acquire gil;
-			pybind11::function overload = py::get_overload(this, "idot");
-			if (overload)
-				return overload.operator()<py::return_value_policy::reference>(xx).cast<double>();
-			else
-				py::pybind11_fail("Tried to call pure virtual function 'idot'.");
-		}
-};
 class PyObjective : public ROL::Objective<double>
 {
 	public:
-		//using ROL::Objective<double>::Objective;
 
-		double value( const ROL::Vector<double> &x, double &tol ) override
+		double value( const ROL::Vector<double>& x, double &tol ) override
 		{
 			py::gil_scoped_acquire gil;
 			pybind11::function overload = py::get_overload(this, "value");
@@ -161,115 +100,117 @@ class PyObjective : public ROL::Objective<double>
 		}
 };
 
-PYBIND11_DECLARE_HOLDER_TYPE(Z, Teuchos::RCP<Z>);
-
 PYBIND11_PLUGIN(ROL)
 {
   py::module m("ROL", "pybind11 ROL plugin");
 
-  py::class_<ROL::Vector<double>>(m, "Vector");
+  py::class_<ROL::Vector<double>, std::shared_ptr<ROL::Vector<double>>>(m, "Vector");
 
   // ROL::StdVector<double>
   //
-  py::class_<ROL::StdVector<double>, ROL::Vector<double>>(m, "StdVector")
-    .def("__init__",
-         [](ROL::StdVector<double> &instance, int n) {
-           Teuchos::RCP<std::vector<double>> tp = Teuchos::rcp<std::vector<double>>(new std::vector<double>(n, 0.0));
-           new (&instance) ROL::StdVector<double>(tp);
-         })
-    .def("norm", &ROL::StdVector<double>::norm)
-    .def("dimension", &ROL::StdVector<double>::dimension)
-	.def("__setitem__", [](ROL::StdVector<double> &vec, const int& idx, const double& val)
-	  {
-	  Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
-		if(idx >= vvec->size())
-		{
-		  throw py::index_error();
-		}else
-		{
-	      (*vvec)[idx] = val;
-		}
-	  }
-	)
-	.def("__getitem__", [](ROL::StdVector<double> &vec, const py::slice& slice)
-	  {
-	    Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
-		py::size_t start, stop, step, slicelength;
-        if (!slice.compute((py::size_t)(vvec->size()), &start, &stop, &step, &slicelength))
-          throw py::error_already_set();
-	    auto res = std::make_shared<std::vector<double>>();
-		res->resize(slicelength);
-	    for (int i = start; i < stop; i=i+step) {
-          res->push_back((*vvec)[i]);
-	    }
-	  }
-	)
-	.def("__getitem__", [](ROL::StdVector<double> &vec, const int& idx)
-	  {
-	  Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
-		if(idx >= vvec->size())
-		{
-		  throw py::index_error();
-		}else
-		{
-	      return (*vvec)[idx];
-		}
-	  }
-	)
-	.def("scale", &ROL::StdVector<double>::scale);
+  //py::class_<ROL::StdVector<double>, ROL::Vector<double>>(m, "StdVector")
+  //  .def("__init__",
+  //       [](ROL::StdVector<double> &instance, int n) {
+  //         Teuchos::RCP<std::vector<double>> tp = Teuchos::rcp<std::vector<double>>(new std::vector<double>(n, 0.0));
+  //         new (&instance) ROL::StdVector<double>(tp);
+  //       })
+  //  .def("norm", &ROL::StdVector<double>::norm)
+  //  .def("dimension", &ROL::StdVector<double>::dimension)
+  //  .def("__setitem__", [](ROL::StdVector<double> &vec, const int& idx, const double& val)
+  //    {
+  //    Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
+  //      if(idx >= vvec->size())
+  //      {
+  //        throw py::index_error();
+  //      }else
+  //      {
+  //        (*vvec)[idx] = val;
+  //      }
+  //    }
+  //  )
+  //  .def("__getitem__", [](ROL::StdVector<double> &vec, const py::slice& slice)
+  //    {
+  //      Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
+  //      py::size_t start, stop, step, slicelength;
+  //      if (!slice.compute((py::size_t)(vvec->size()), &start, &stop, &step, &slicelength))
+  //        throw py::error_already_set();
+  //      auto res = std::make_shared<std::vector<double>>();
+  //      res->resize(slicelength);
+  //      for (int i = start; i < stop; i=i+step) {
+  //        res->push_back((*vvec)[i]);
+  //      }
+  //    }
+  //  )
+  //  .def("__getitem__", [](ROL::StdVector<double> &vec, const int& idx)
+  //    {
+  //    Teuchos::RCP<std::vector<double>> vvec = vec.getVector();
+  //      if(idx >= vvec->size())
+  //      {
+  //        throw py::index_error();
+  //      }else
+  //      {
+  //        return (*vvec)[idx];
+  //      }
+  //    }
+  //  )
+  //  .def("scale", &ROL::StdVector<double>::scale);
 
   // EigenVector
   //
-  py::class_<EigenVector, ROL::Vector<double>>(m, "EigenVector", py::buffer_protocol())
-	.def(py::init<const int>())
-	.def(py::init<const py::array_t<double>>())
-	.def("norm", &EigenVector::norm)
-	.def("dimension", &EigenVector::dimension)
-	.def("__setitem__", [](EigenVector &vec, const int& idx, const double& val)
-	  {
-	  auto vvec = vec.getVector();
-		if(idx >= vvec->size())
-		{
-		  throw py::index_error();
-		}else
-		{
-		  (*vvec)[idx] = val;
-		}
-	  }
-	)
-	.def("__getitem__", [](EigenVector &vec, const int& idx)
-	  {
-	  auto vvec = vec.getVector();
-		if(idx >= vvec->size())
-		{
-		  throw py::index_error();
-		}else
-		{
-		  return (*vvec)[idx];
-		}
-	  }
-	)
-	.def("scale", &EigenVector::scale);
+  //py::class_<EigenVector, ROL::Vector<double>>(m, "EigenVector", py::buffer_protocol())
+  //  .def(py::init<const int>())
+  //  .def(py::init<const py::array_t<double>>())
+  //  .def("norm", &EigenVector::norm)
+  //  .def("dimension", &EigenVector::dimension)
+  //  .def("__setitem__", [](EigenVector &vec, const int& idx, const double& val)
+  //    {
+  //    auto vvec = vec.getVector();
+  //      if(idx >= vvec->size())
+  //      {
+  //        throw py::index_error();
+  //      }else
+  //      {
+  //        (*vvec)[idx] = val;
+  //      }
+  //    }
+  //  )
+  //  .def("__getitem__", [](EigenVector &vec, const int& idx)
+  //    {
+  //    auto vvec = vec.getVector();
+  //      if(idx >= vvec->size())
+  //      {
+  //        throw py::index_error();
+  //      }else
+  //      {
+  //        return (*vvec)[idx];
+  //      }
+  //    }
+  //  )
+  //  .def("scale", &EigenVector::scale);
 
   //py::class_<CustomLA, ROL::Vector<double>>(m, "CustomLA")
-  py::class_<CustomLA, Teuchos::RCP<CustomLA>>(m, "CustomLA")
+  py::class_<CustomLA, ROL::Vector<double>, std::shared_ptr<CustomLA>>(m, "CustomLA")
 	//.def(py::init<std::function<void(CustomLA, CustomLA)>, std::function<void(CustomLA, double)>>())
 	.def(py::init<>())
 	//.def("norm", &EigenVector::norm)
 	//.def("dimension", &EigenVector::dimension)
-	.def("plus", &CustomLA::plus)
-	.def("scale", &CustomLA::scale)
-	.def("iclone", &CustomLA::iclone)
-	.def("clone", &CustomLA::clone, py::return_value_policy::reference)
-	.def("checkVector", [](CustomLA* x, CustomLA* y, CustomLA* z)
+	//.def("plus", &CustomLA::plus)
+	//.def("scale", &CustomLA::scale)
+	.def("clone", &CustomLA::clone)
+	.def("norm", &CustomLA::norm)
+	.def("checkVector", [](std::shared_ptr<CustomLA>& x, std::shared_ptr<CustomLA>& y, std::shared_ptr<CustomLA>& z)
 	  {
 	    x->checkVector(*y, *z, true, std::cout);
 	  })
-    .def("checkClone", [](CustomLA* instance) 
+    .def("checkScale", [](std::shared_ptr<CustomLA>& instance) 
 	  {
 	    instance->scale(0.5);
-	    instance->iclone()->scale(0.5);
-		
+	  })
+    .def("checkClone", [](std::shared_ptr<CustomLA>& instance) 
+	  {
+	    instance->scale(0.5);
+	    instance->clone()->scale(0.5);
+		instance->scale(1.0);
 	  });
 	//.def("idot", &CustomLA::idot)
 	//.def("copy_data_from", &CustomLA::copy_data_from);
