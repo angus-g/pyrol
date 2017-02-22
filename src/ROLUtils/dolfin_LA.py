03763024c8f6c84125ff512bac7503b7bb61e059
Chris Richardson <chris@bpi.cam.ac.uk>
2017-02-22 12:59:45 +0000

    Work on setup.py

--- src/ROLUtils/dolfin_LA.py
+++ src/ROLUtils/dolfin_LA.py
@@ -1,5 +1,4 @@
 import ROL
-# from dolfin import as_backend_type
 
 class dolfinLA(ROL.CustomLA):
     def __init__(self, vec, inner=None):
@@ -8,13 +7,9 @@ class dolfinLA(ROL.CustomLA):
         self.inner = inner
 
     def plus(self, x):
-        # PETSc doesn't like adding vector to themselves,
-        # so we have to add this check
-  #      xpet = as_backend_type(x.vec).vec()
-#        thispet = as_backend_type(self.vec).vec()
- #       if xpet == thispet:
-#            self.vec *= 2
-#        else:
+        # PETSc doesn't like adding a vector to itself,
+        # so we have to make a copy. Would be good to fix this
+        # upstream in DOLFIN
         self.vec += x.vec.copy()
 
     def scale(self, alpha):
@@ -40,7 +35,7 @@ class dolfinLA(ROL.CustomLA):
         return len(self.vec)
 
     def basis(self, i):
-        res = dolfinLA(self.vec.copy(), self.inner)
+        res = self.clone()
         res.scale(0.0)
         res[i] = 1.0
         return res
