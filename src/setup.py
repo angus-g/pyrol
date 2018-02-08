19df07ea6612679756760972da6abd3b5dbc2346
florianwechsung <f.wechsung@gmail.com>
2018-02-08 23:50:45 +1100

    updated setup.py to use cmake and to find trilinos automatically

--- src/setup.py
+++ src/setup.py
@@ -1,73 +1,82 @@
-from __future__ import print_function
-import os, sys, subprocess
+import os
+import re
+import sys
+import platform
+import subprocess
+
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
-if "TRILINOS_DIR" not in os.environ:
-    print("Please set the TRILINOS_DIR environment variable to point to the Trilinos")
-    print("installation directory containing ROL and Teuchos\n")
-    sys.exit(1)
+class TrilinosInstallation(object):
 
-trilinos_dir = os.environ["TRILINOS_DIR"]
+    def __init__(self, user=False):
+        self.user = user
 
-if (sys.platform == 'darwin'):
-    os.environ.setdefault('LDFLAGS','')
-    os.environ['LDFLAGS'] += os.path.expandvars(' -Wl,-rpath,$TRILINOS_DIR/lib')
+    def get_path(self):
+        import roltrilinos
+        return roltrilinos.get_trilinos_path()
 
-try:
-    import pybind11
-except ImportError:
-    raise ImportError("Please pip install pybind11 first")
-else:
-    pybind11_get_include = pybind11.get_include()
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
-# Try to get the MPI flags
-try:
-    mpicxx = subprocess.check_output(["mpicxx", "-show"])
-    ex_flags = mpicxx.strip()
-    ex_flags = ex_flags.decode().split(" ")[1:]
-except IOError:
-    print('Cannot query mpicxx compiler used to compile Trilinos\n')
-    print('Make sure that mpicxx exists, and returns flags with "mpicxx -show"')
-    sys.exit(1)
 
-# Make sure we are using c++11
-ex_flags.append("-std=c++11")
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
 
-# Collect up libraries needed by mpi
-ex_libs = [x[2:] for x in ex_flags if x[:2]=='-l']
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
-if not os.path.exists(trilinos_dir+"/include/ROL_config.h"):
-    print("Cannot find Trilinos include directory with ROL. Make sure TRILINOS_DIR is set correctly.\n")
-    sys.exit(1)
+        for ext in self.extensions:
+            self.build_extension(ext)
 
-ex_libs.extend(["teuchoscore", "teuchosnumerics", "teuchosparameterlist", "teuchoscomm"])
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir + '\\' + ext.name,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-setup(name="PyROL",
-      version="0.1.1",
-      author="Chris Richardson and Florian Wechsung",
-      author_email="pyrol-dev@googlemail.com",
-      license="LGPLv3",
-      packages=['ROL'],
-      # setup_requires=["pybind11"],
-      ext_modules=[
-          Extension("_ROL",
-                    ["ROL/ROL.cpp",
-                     "ROL/algorithm.cpp",
-                     "ROL/algorithmstate.cpp",
-                     "ROL/bounds.cpp",
-                     "ROL/constraint.cpp",
-                     "ROL/moreauyosidapenalty.cpp",
-                     "ROL/objective.cpp",
-                     "ROL/optimizationproblem.cpp",
-                     "ROL/optimizationsolver.cpp",
-                     "ROL/parameterlist.cpp",
-                     "ROL/stdvector.cpp",
-                     "ROL/vector.cpp"],
-                    define_macros = [("ROL_SHARED_POINTER", 1)],
-                    include_dirs = [pybind11_get_include] + [trilinos_dir+"/include"],
-                    library_dirs = [trilinos_dir+"/lib"],
-                    runtime_library_dirs = [trilinos_dir+"/lib"],
-                    libraries = ex_libs,
-                    extra_compile_args = ex_flags,
-                 )],
-      )
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+        trilinos_installation = TrilinosInstallation()
+        trilinos_dir = trilinos_installation.get_path()
+
+        cmake_args += ['-DTRILINOS_DIR:Path=' + trilinos_dir]
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+setup(
+    name='ROL',
+    version='0.0.1',
+    author='Chris Richardson, Greg von Winckel, Florian Wechsung',
+    author_email='wechsung@maths.ox.ac.uk',
+    description='A python wrapper for the ROL package.',
+    long_description='',
+    ext_modules=[CMakeExtension('ROL')],
+    packages=['ROL'],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
