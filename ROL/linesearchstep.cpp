c78b850197b7c4249b51315579403baf3ebe8405
Angus Gibson <angus@agibson.me>
2021-02-08 12:32:03 +1100

    Create custom InitBFGS class
    This is basically a re-implementation of ROL::lBFGS : ROL::Secant. As
    implemented in ROL itself, the lBFGS::applyH() method calls
    Secant::applyH0() to get the action of the initial inverse Hessian
    estimate. This means that even if we create a subclass of lBFGS, it
    won't use any overriden applyH0 method.
    
    Our new InitBFGS class trampolines through PyInitBFGS, which defines
    applyH0 as an overridable virtual function. We can then override the
    action of the initial inverse Hessian alone, and still retain the rest
    of the BFGS/optimisation infrastructure.
    
    Additionally, we can't defer to pybind's constructor binding for
    ROL::LineSearchStep, as it won't downcast an InitBFGS (or child
    thereof) to a ROL::Secant.

--- ROL/linesearchstep.cpp
+++ ROL/linesearchstep.cpp
@@ -1,50 +1,67 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
+#include "py_shared_ptr.hpp"
+PYBIND11_DECLARE_HOLDER_TYPE(T, py_shared_ptr<T>);
 
 #include <ROL_LineSearchStep.hpp>
 #include <ROL_Secant.hpp>
 
-class PySecant : public ROL::Secant<double> {
+class InitBFGS : public ROL::Secant<double> {
 public:
   using ROL::Secant<double>::Secant;
 
   void applyH(ROL::Vector<double> &Hv, const ROL::Vector<double> &v) const override {
-    PYBIND11_OVERRIDE_PURE(void, ROL::Secant<double>, applyH, Hv, v);
+    const auto& state = ROL::Secant<double>::get_state();
+    double zero(0);
+
+    Hv.set(v.dual());
+    std::vector<double> alpha(state->current+1, zero);
+    for (int i = state->current; i >= 0; i--) {
+      alpha[i] = state->iterDiff[i]->dot(Hv);
+      alpha[i] /= state->product[i];
+
+      Hv.axpy(-alpha[i], (state->gradDiff[i])->dual());
+    }
+
+    auto tmp = Hv.clone();
+    applyH0(*tmp, Hv.dual());
+    Hv.set(*tmp);
+
+    double beta(0);
+    for (int i = 0; i <= state->current; i++) {
+      beta = Hv.dot((state->gradDiff[i])->dual());
+      beta /= state->product[i];
+
+      Hv.axpy(alpha[i] - beta, *(state->iterDiff[i]));
+    }
   }
 
   void applyB(ROL::Vector<double> &Bv, const ROL::Vector<double> &v) const override {
-    PYBIND11_OVERRIDE_PURE(void, ROL::Secant<double>, applyB, Bv, v);
+
   }
+};
+
+class PyInitBFGS : public InitBFGS {
+public:
+  using InitBFGS::InitBFGS;
 
   void applyH0(ROL::Vector<double> &Hv, const ROL::Vector<double> &v) const override {
     PYBIND11_OVERRIDE(void, ROL::Secant<double>, applyH0, Hv, v);
   }
 };
 
-class PyLineSearch : public ROL::LineSearch<double> {
-public:
-  using ROL::LineSearch<double>::LineSearch;
-
-  void run(double &alpha, double &fval, int &ls_neval, int &ls_ngrad,
-	   const double &gs, const ROL::Vector<double> &s, const ROL::Vector<double> &x,
-	   ROL::Objective<double> &obj, ROL::BoundConstraint<double> &con) override {
-    PYBIND11_OVERRIDE_PURE(void, ROL::LineSearch<double>, run, alpha, fval, ls_neval, ls_ngrad, gs, s, x, obj, con);
-  }
-};
-
 void init_linesearchstep(py::module& m) {
-  py::class_<ROL::Secant<double>, PySecant>(m, "Secant")
+  py::class_<ROL::Secant<double>, std::shared_ptr<ROL::Secant<double>>>(m, "Secant");
+
+  // class, trampoline, reference type and parent class
+  py::class_<InitBFGS, PyInitBFGS, py_shared_ptr<InitBFGS>, ROL::Secant<double>>(m, "InitBFGS")
     .def(py::init<>())
     .def("applyH0", &ROL::Secant<double>::applyH0);
 
-  py::class_<ROL::LineSearch<double>, PyLineSearch>(m, "LineSearch")
-    .def(py::init<ROL::ParameterList&>());
+  py::class_<ROL::Step<double>, std::shared_ptr<ROL::Step<double>>>(m, "Step");
 
-  py::class_<ROL::LineSearchStep<double>>(m, "LineSearchStep")
-    .def(py::init<ROL::ParameterList&,
-	 std::shared_ptr<ROL::LineSearch<double>>,
-	 std::shared_ptr<ROL::Secant<double>>>(),
-	 py::arg("parlist"),
-	 py::arg("linesearch") = (ROL::LineSearch<double>*)nullptr,
-	 py::arg("secant") = (ROL::Secant<double>*)nullptr);
+  py::class_<ROL::LineSearchStep<double>, std::shared_ptr<ROL::LineSearchStep<double>>, ROL::Step<double>>(m, "LineSearchStep")
+    .def(py::init([](ROL::ParameterList &params, std::shared_ptr<ROL::Secant<double>> &secant) {
+      return new ROL::LineSearchStep<double>(params, nullptr, secant);
+    }));
 }
