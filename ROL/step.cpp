24325ea0ec80ec2c12c990828a81e9f9ec053055
Angus Gibson <angus@agibson.me>
2021-02-12 10:38:01 +1100

    Add binding to ROL::TrustRegionStep
    This allows for access to more optimisation strategies. Using this
    with a customised Secant method requires a patched (or more recent
    than the pypi roltrilinos package) version of Trilinos/ROL, because
    ROL::TrustRegionStep itself ignores any secant we pass into it.

--- ROL/step.cpp
+++ ROL/step.cpp
@@ -2,6 +2,7 @@
 namespace py = pybind11;
 
 #include <ROL_LineSearchStep.hpp>
+#include <ROL_TrustRegionStep.hpp>
 
 void init_step(py::module& m) {
   py::class_<ROL::Step<double>, std::shared_ptr<ROL::Step<double>>>(m, "Step");
@@ -10,4 +11,7 @@ void init_step(py::module& m) {
     .def(py::init([](ROL::ParameterList &params, std::shared_ptr<ROL::Secant<double>> &secant) {
       return new ROL::LineSearchStep<double>(params, nullptr, secant);
     }));
+
+  py::class_<ROL::TrustRegionStep<double>, std::shared_ptr<ROL::TrustRegionStep<double>>, ROL::Step<double>>(m, "TrustRegionStep")
+    .def(py::init<std::shared_ptr<ROL::Secant<double>>&, ROL::ParameterList&>());
 }
