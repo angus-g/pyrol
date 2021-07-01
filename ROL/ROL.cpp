fe76a8707de98c6d4412f601a9dbf75217f90ec2
Angus Gibson <angus@agibson.me>
2021-07-02 09:53:20 +1000

    Start porting to ROL 2.0 interface
    There are a few minor changes here, but the biggest thing is that
    OptimizationProblem and OptimizationSolver become Problem and Solver
    respectively. These act mostly the same as their predecessors, but
    support a few slightly different methods (e.g. for setting bounds).

--- ROL/ROL.cpp
+++ ROL/ROL.cpp
@@ -1,18 +1,17 @@
-
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
 
-void init_algorithm(py::module&);
 void init_algorithmstate(py::module&);
 void init_bounds(py::module&);
 void init_constraint(py::module&);
-void init_moreauyosidapenalty(py::module&);
 void init_objective(py::module&);
 void init_stdvector(py::module&);
 void init_vector(py::module&);
 void init_optimizationproblem(py::module&);
 void init_optimizationsolver(py::module&);
+void init_problem(py::module&);
+void init_solver(py::module&);
 void init_parameterlist(py::module&);
 
 PYBIND11_MODULE(_ROL, m) {
@@ -22,14 +21,14 @@ PYBIND11_MODULE(_ROL, m) {
   m.attr("__version__") = "0.1.1";
 
   init_vector(m);
-  init_algorithm(m);
   init_algorithmstate(m);
   init_bounds(m);
   init_constraint(m);
   init_objective(m);
-  init_moreauyosidapenalty(m);
   init_stdvector(m);
   init_optimizationproblem(m);
   init_optimizationsolver(m);
+  init_problem(m);
+  init_solver(m);
   init_parameterlist(m);
 }
---
+++ ROL/solver.cpp
@@ -0,0 +1,27 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+#include <map>
+
+#include <ROL_StatusTest.hpp>
+#include <ROL_Solver.hpp>
+
+void init_solver(py::module &m) {
+  py::class_<ROL::StatusTest<double>,
+	     std::shared_ptr<ROL::StatusTest<double>>>(m, "StatusTest")
+    .def(py::init<ROL::ParameterList&>());
+
+  py::class_<ROL::Solver<double>,
+	     std::shared_ptr<ROL::Solver<double>>>(m, "Solver")
+    .def(py::init<std::shared_ptr<ROL::Problem<double>>&, ROL::ParameterList&>())
+    .def("solve",
+	 [](ROL::Solver<double> &instance, std::shared_ptr<ROL::StatusTest<double>> &status, bool print_output) {
+	   if (print_output)
+	     instance.solve(std::cout, status);
+	   else
+	     instance.solve(status);
+	 },
+	 py::arg("status") = (ROL::StatusTest<double>*)nullptr,
+	 py::arg("print_output") = true)
+    .def("getAlgorithmState", &ROL::Solver<double>::getAlgorithmState);
+}
