e4ec77e66baa561308e62738427a3f503c9b9249
Angus Gibson <angus@agibson.me>
2021-09-13 14:53:31 +1000

    Add ROL 2.0 Problem constraints and finalize
    We add constraints to the Problem after construction (rather than part
    of the constructor itself). Because the function for inequality
    constraints is overloaded from the general constraint function, we add
    a specialisation.

--- ROL/problem.cpp
+++ ROL/problem.cpp
@@ -11,5 +11,20 @@ void init_problem(py::module &m) {
     .def(py::init<std::shared_ptr<ROL::Objective<double>>&,
 	 ROL::Ptr<ROL::Vector<double>>&,
 	 ROL::Ptr<ROL::Vector<double>>&>(),
-	 py::arg("obj"), py::arg("x"), py::arg("g") = (ROL::Vector<double>*)nullptr);
+	 py::arg("obj"), py::arg("x"), py::arg("g") = (ROL::Vector<double>*)nullptr)
+    .def("addBoundConstraint", &ROL::Problem<double>::addBoundConstraint)
+    .def("addInequalityConstraint",
+	 py::overload_cast<
+	 std::string,
+	 const ROL::Ptr<ROL::Constraint<double>>&,
+	 const ROL::Ptr<ROL::Vector<double>>&,
+	 const ROL::Ptr<ROL::BoundConstraint<double>>&,
+	 const ROL::Ptr<ROL::Vector<double>>&,
+	 bool>(&ROL::Problem<double>::addConstraint),
+	 py::arg("name"), py::arg("icon"), py::arg("imul"), py::arg("ibnd"),
+	 py::arg("ires") = (ROL::Vector<double>*)nullptr,
+	 py::arg("reset") = false)
+    .def("finalize", [](ROL::Problem<double> &instance) {
+      instance.finalize(false, true);
+    });
 }
