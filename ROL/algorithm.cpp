07a328b962c49c4c9d0fc67d9f80ebefe3cbb5f7
Angus Gibson <angus@agibson.me>
2022-05-25 11:01:28 +1000

    Add interface to TypeB QuasiNewtonAlgorithm

--- ROL/algorithm.cpp
+++ ROL/algorithm.cpp
@@ -2,6 +2,7 @@
 namespace py = pybind11;
 
 #include <ROL_TypeB_LinMoreAlgorithm.hpp>
+#include <ROL_TypeB_QuasiNewtonAlgorithm.hpp>
 #include <ROL_Bounds.hpp>
 
 void init_algorithm(py::module &m) {
@@ -13,5 +14,15 @@ void init_algorithm(py::module &m) {
 	 [](ROL::TypeB::LinMoreAlgorithm<double> &instance, ROL::Vector<double> &x,
 	    ROL::Objective<double> &obj, ROL::Bounds<double> &bnd) {
 	   instance.run(x, obj, bnd, std::cout);
+	 });
+
+  py::class_<ROL::TypeB::QuasiNewtonAlgorithm<double>>(m, "QuasiNewtonAlgorithm")
+    .def(py::init<ROL::ParameterList&>())
+    .def(py::init<ROL::ParameterList&, const std::shared_ptr<ROL::Secant<double>>&>())
+    .def("setStatusTest", &ROL::TypeB::QuasiNewtonAlgorithm<double>::setStatusTest)
+    .def("run",
+	 [](ROL::TypeB::QuasiNewtonAlgorithm<double> &instance, ROL::Vector<double> &x,
+	    ROL::Objective<double> &obj, ROL::Bounds<double> &bnd) {
+	   instance.run(x, obj, bnd,std::cout);
 	 });
 }
