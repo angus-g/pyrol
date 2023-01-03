eeab5a019375be4d9ceba780d658e2e6e109ef42
Angus Gibson <angus@agibson.me>
2023-01-04 10:31:15 +1100

    Bump project version to 0.4.0

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ class CMakeBuild(build_ext):
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_temp)
 
 setup(
-    name='ROL',
-    version='0.3.0',
+    name='pyroltrilinos',
+    version='0.4.0',
     author='Chris Richardson, Greg von Winckel, Florian Wechsung, Angus Gibson',
     author_email='angus@agibson.me',
     description='A python wrapper for the ROL package.',
