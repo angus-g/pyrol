f1c99327a0db386c6ee3a7dad3e0043c588d567d
Angus Gibson <angus@agibson.me>
2023-12-18 10:59:07 +1100

    Explicitly specify Python include directory for Boost build

--- tools/build_boost.sh
+++ tools/build_boost.sh
@@ -6,5 +6,7 @@ wget "https://github.com/boostorg/boost/releases/download/boost-${VERSION}/boost
 tar xf boost-${VERSION}.tar.xz
 cd boost-${VERSION}
 echo "using darwin : : $CXX ;" > user-config.jam
+python_include=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["include"])')
+echo "using python : : : $python_include ;" >> user-config.jam
 ./bootstrap.sh --prefix=/usr/local
 ./b2 --prefix=/usr/local --user-config=user-config.jam
