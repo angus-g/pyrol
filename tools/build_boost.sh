a062108b94f5180b2dcfe2dd0576ca926ad92854
Angus Gibson <angus@agibson.me>
2023-12-18 11:50:24 +1100

    Actually install macOS Boost to /usr/local

--- tools/build_boost.sh
+++ tools/build_boost.sh
@@ -2,11 +2,12 @@
 
 VERSION=1.83.0
 
-wget "https://github.com/boostorg/boost/releases/download/boost-${VERSION}/boost-${VERSION}.tar.xz"
+[ ! -f "boost-${VERSION}.tar.xz" ] && wget "https://github.com/boostorg/boost/releases/download/boost-${VERSION}/boost-${VERSION}.tar.xz"
+[ -d "boost-${VERSION}" ] && rm -rf "boost-${VERSION}"
 tar xf boost-${VERSION}.tar.xz
 cd boost-${VERSION}
 echo "using darwin : : $CXX ;" > user-config.jam
 python_include=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["include"])')
 echo "using python : : : $python_include ;" >> user-config.jam
 ./bootstrap.sh --prefix=/usr/local
-./b2 --prefix=/usr/local --user-config=user-config.jam
+sudo ./b2 --prefix=/usr/local --user-config=user-config.jam install
