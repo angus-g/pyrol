d739782dc7fb363fd3520c3f8d7c431391ef51a5
Angus Gibson <angus@agibson.me>
2024-05-02 10:04:16 +1000

    Bump boost version for macOS builds

--- tools/build_boost.sh
+++ tools/build_boost.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-VERSION=1.83.0
+VERSION=1.85.0
+SUFFIX=-b2-nodocs
 
-[ ! -f "boost-${VERSION}.tar.xz" ] && wget "https://github.com/boostorg/boost/releases/download/boost-${VERSION}/boost-${VERSION}.tar.xz"
+[ ! -f "boost-${VERSION}.tar.xz" ] && wget "https://github.com/boostorg/boost/releases/download/boost-${VERSION}/boost-${VERSION}${SUFFIX}.tar.xz"
 [ -d "boost-${VERSION}" ] && rm -rf "boost-${VERSION}"
-tar xf boost-${VERSION}.tar.xz
+tar xf boost-${VERSION}${SUFFIX}.tar.xz
 cd boost-${VERSION}
 echo "using darwin : : $CXX ;" > user-config.jam
 python_include=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["include"])')
